600230981fcdd6206a19bb02ace5758894e8b297
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 23:10:04 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("---Adivinha---");
+
+    Console.WriteLine();
+
+    Console.WriteLine("O que é, o que é...");
+        Thread.Sleep(1000);
+
+    Console.WriteLine("...Cai em pé e corre deitado?");
+        Thread.Sleep(5000);
+
+    Console.WriteLine();
+
+    Console.WriteLine("A chuva.");
+
+    Console.WriteLine();
+
+
+
+
+
+
+
+
+
